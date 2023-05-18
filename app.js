5e8b896df06f2d686712a1ec1b68619d6660731a
sihar <luq5nvd@gmail.com>
2023-05-18 10:48:21 +0700

    bcrypt - salt + hash

--- app.js
+++ app.js
@@ -5,11 +5,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const ejs = require('ejs')
 const mongoose = require('mongoose')
-const md5 = require('md5')
+const bcrypt = require('bcrypt')
+const saltRounds = 10;
 
 const app = express()
-
-console.log(md5("123456"));
 
 app.use(express.static('public'))
 
@@ -52,38 +51,48 @@ app.get("/register", (req, res) => {
     res.render("register")
 })
 
-app.post("/register",async (req, res) => {
-    const newUser = new User({
-        email: req.body.username,
-        password:md5(req.body.password)
-    })
+app.post("/register", async (req, res) => {
+    
+    bcrypt.hash(req.body.password, saltRounds, async(err, hash) =>{
+        // Store hash in your password DB.
+        const newUser = new User({
+            email: req.body.username,
+            password:hash
+        })
 
-    const saveNewUser = await newUser.save()
-    if (newUser === saveNewUser) {
-        console.log(`${saveNewUser} successfully saved to database`);
-        res.render("secrets")
-    } else {
-        console.log("cant save newUser to database");
+        const saveNewUser = await newUser.save()
 
-    }
+        if (newUser === saveNewUser) {
+            console.log(`${saveNewUser} successfully saved to database`);
+            res.render("secrets")
+        } else {
+            console.log("cant save newUser to database");
+    
+        }
+    });
+
+    
 })
 
 app.post("/login",async (req, res) => {
     const username = req.body.username
-    const password = md5(req.body.password)
+    const password = req.body.password
 
     const doc = await User.findOne({
         email:username
     }).exec()
 
     if (username === doc.email) {
-        if (password === doc.password) {
-            console.log(`${doc} is found`);
-            res.render("secrets")
-        } else {
-            console.log(`${password} is wrong password`);
-        }
-        
+        // Load hash from your password DB.
+        bcrypt.compare(password, doc.password, function(err, result) {
+            // result == true
+            if (result) {
+                console.log(`${doc} is found`);
+                res.render("secrets")
+            } else {
+                console.log(`${password} is wrong password`);
+            }
+        });
     } else {
         console.log(`${username} not found, please register`);
     }
